febed569260e220070de41dada3043a6be9f9bd1
c0dew0rm <rahulgenus007@gmail.com>
2019-11-17 16:05:38 +0530

    Passing Data via services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,44 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes:Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png' ),
-    new Recipe('Another Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png' )
+    new Recipe(
+        'Chicken Korma',
+        'A super tasty recipe cooked with minced chicken.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Chicken_Korma_%286110150046%29.jpg/800px-Chicken_Korma_%286110150046%29.jpg',
+        [
+            new Ingredient('Chicken', 1),
+            new Ingredient('Tomatoes', 20),
+            new Ingredient('Onion', 40)
+        ]
+        ),
+    new Recipe(
+        'Big Fat Burger',
+        'Yummy Burger',
+        'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__340.jpg',
+        [
+            new Ingredient('Chicken', 1),
+            new Ingredient('Bun', 2),
+            new Ingredient('Onion', 40)
+        ]
+        )
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 
 }
